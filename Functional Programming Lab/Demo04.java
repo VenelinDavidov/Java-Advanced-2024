a839de59d479559436b502f0d7a1dfcf59bbd4d6
VenelinDavidov <114108014+VenelinDavidov@users.noreply.github.com>
2024-01-24 22:53:20 +0200

    Create Demo04.java

---
+++ Functional Programming Lab/Demo04.java
@@ -0,0 +1,29 @@
+package TrainerDemo;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Demo04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //1. входни данни: списък с дробни числа
+        List<Double> prices = Arrays.stream(scanner.nextLine()
+                        .split(", "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
+        //цена -> върна цена + 20%
+        Function<Double, Double> addVAT = price -> price * 1.2;
+        //цена с ДДС -> отпечатвам
+        Consumer<Double> printFormatPrice = price -> System.out.printf("%.2f%n", price);
+
+        System.out.println("Prices with VAT:");
+        for (double price : prices) {
+            double priceWithVAT = addVAT.apply(price); //цена след добавено ДДС
+            printFormatPrice.accept(priceWithVAT);
+        }
+    }
+}
