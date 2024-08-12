9d743b2a599a52dbcd30da2245243fbd6fc21f73
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2024-08-12 21:30:21 +0300

    Java solution for Stream,Files and Directories Exercises

--- Streams_Files_And_Directories/src/Lab_Streams_Files_And_Directories/SerializeCustomObject_09.java
+++ Streams_Files_And_Directories/src/Lab_Streams_Files_And_Directories/SerializeCustomObject_09.java
@@ -4,32 +4,32 @@ import java.io.*;
 
 public class SerializeCustomObject_09 {
 
-public static class Cube implements Serializable {
+    public static class Cube implements Serializable {
 
-    String color;
-    double width;
-    double height;
-    double depth;
+        String color;
+        double width;
+        double height;
+        double depth;
 
-}
+    }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-    Cube cube  = new Cube();
+        Cube cube = new Cube();
 
-    cube.color = "green";
-    cube.width = 15.3;
-    cube.height = 42;
-    cube.depth = 3;
+        cube.color = "green";
+        cube.width = 15.3;
+        cube.height = 42;
+        cube.depth = 3;
 
 
-       ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("ice_cube.txt"));
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("ice_cube.txt"));
 
-       objectOutputStream.writeObject(cube);
+        objectOutputStream.writeObject(cube);
 
-       ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("ice_cube.txt"));
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("ice_cube.txt"));
 
-       Cube iceCube = (Cube) objectInputStream.readObject();
+        Cube iceCube = (Cube) objectInputStream.readObject();
 
     }
 }
