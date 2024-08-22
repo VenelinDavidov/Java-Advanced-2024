18e4041eaaade2a48877f10a76c872551da730be
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2024-08-22 22:32:28 +0300

    Java solution Generics

--- Exercise Generics/src/Box/Main.java
+++ Exercise Generics/src/Box/Main.java
@@ -1,4 +1,27 @@
 package Box;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Double> box = new Box<Double>();
+
+        for (int i = 0; i < n; i++) {
+            Double element = Double.parseDouble(scanner.nextLine());
+            box.add(element);
+        }
+
+//        String[] indices = scanner.nextLine().split(" ");
+//        int firstIndex = Integer.parseInt(indices[0]);
+//        int secondIndex = Integer.parseInt(indices[1]);
+//        box.swap(firstIndex, secondIndex);
+
+        Double elementToCompare = Double.parseDouble(scanner.nextLine());
+        System.out.println(box.countOfGreaterItems(elementToCompare));
+    }
 }
+
+
