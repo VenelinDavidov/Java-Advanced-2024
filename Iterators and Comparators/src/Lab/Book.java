def1f40f80d2bd3b39a8bc720b427b4192231408
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2024-08-25 09:58:20 +0300

    Java solution for iterators and comparators

--- Iterators and Comparators/src/Lab/Book.java
+++ Iterators and Comparators/src/Lab/Book.java
@@ -1,4 +1,70 @@
 package Lab;
 
-public class Book {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Book implements Comparable<Book> {
+
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    // Generate constructor
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    //getter and setter
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    private void setAuthors(String... authors) {
+        this.authors = Arrays.stream(authors).collect(Collectors.toList());
+        // Arrays.asList(authors)
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+
+    // Implements method compareTo (interface Comparable)
+    @Override
+    public int compareTo(Book other) {
+
+        int result = this.title.compareTo(other.title);
+        if (result == 0) {
+            result = Integer.compare(this.year, other.year);
+        }
+        return result;
+    }
+
+
+
+    // Compare by Author-> instrument
+    public static class BookComparatorByAuthorsCount implements Comparator<Book> {
+
+        @Override
+        public int compare(Book first, Book second) {
+            return Integer.compare(first.getAuthors().size(), second.getAuthors().size());
+        }
+    }
+
 }
---
+++ Iterators and Comparators/src/Lab/Main.java
@@ -0,0 +1,41 @@
+package Lab;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book bookOne = new Book("Animal Farm", 3, "George Orwell");
+        Book bookTwo = new Book("The Documents in the Case", 43, "Dorothy Sayers", "Robert Eustace");
+        Book bookThree = new Book("The Documents in the Case", 13);
+
+        if (bookOne.compareTo(bookTwo) < 0) {
+            System.out.printf("%s is before %s%n", bookTwo.getTitle(), bookTwo.getTitle());
+        } else if (bookOne.compareTo(bookTwo) > 0) {
+            System.out.printf("%s is before %s%n", bookTwo.getTitle(), bookOne.getTitle());
+        } else {
+            System.out.println("Book are equal");
+        }
+
+        List<Book> books = new ArrayList<>(List.of(bookThree, bookOne, bookTwo));
+
+        books.sort(new BookComparator());
+
+        books.forEach(b -> System.out.println(b.getTitle()));
+
+        books.sort(new BookComparatorByYear());
+
+        books.forEach(b -> System.out.println(b.getTitle()));
+
+        books.sort(Comparator.comparingInt(b -> b.getAuthors().size()));
+
+        books.forEach(b -> System.out.println(b.getTitle()));
+
+        books.sort(new Book.BookComparatorByAuthorsCount());
+
+    }
+
+}
